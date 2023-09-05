3bf5ef50c58b6154d7d630d84112280e51dd5a6c
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-09-05 23:38:22 +0300

    Code refactoring

--- include/private/plugins/mb_expander.h
+++ include/private/plugins/mb_expander.h
@@ -237,6 +237,9 @@ namespace lsp
                 static size_t                       select_fft_rank(size_t sample_rate);
                 static void                         process_band(void *object, void *subject, size_t band, const float *data, size_t sample, size_t count);
 
+            protected:
+                void                do_destroy();
+
             public:
                 explicit mb_expander(const meta::plugin_t *metadata, bool sc, size_t mode);
                 virtual ~mb_expander() override;
