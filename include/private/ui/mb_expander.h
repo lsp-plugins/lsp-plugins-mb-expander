e95ab27dbd6d2a13711e5ef0e2be40e5f1e2bdcc
bor-T <bor-tattoo@ya.ru>
2023-12-10 19:07:26 +0200

    UPD

--- include/private/ui/mb_expander.h
+++ include/private/ui/mb_expander.h
@@ -2,24 +2,24 @@
  * Copyright (C) 2023 Linux Studio Plugins Project <https://lsp-plug.in/>
  *           (C) 2023 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
- * This file is part of lsp-plugins-mb-dyna-processor
+ * This file is part of lsp-plugins-mb-expander
  * Created on: 6 дек. 2023 г.
  *
- * lsp-plugins-mb-dyna-processor is free software: you can redistribute it and/or modify
+ * lsp-plugins-mb-expander is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * lsp-plugins-mb-dyna-processor is distributed in the hope that it will be useful,
+ * lsp-plugins-mb-expander is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with lsp-plugins-mb-dyna-processor. If not, see <https://www.gnu.org/licenses/>.
+ * along with lsp-plugins-mb-expander. If not, see <https://www.gnu.org/licenses/>.
  */
-#ifndef PRIVATE_UI_MB_DYNA_PROCESSOR_H_
-#define PRIVATE_UI_MB_DYNA_PROCESSOR_H_
+#ifndef PRIVATE_UI_MB_EXPANDER_H_
+#define PRIVATE_UI_MB_EXPANDER_H_
 
 #include <lsp-plug.in/plug-fw/ui.h>
 #include <lsp-plug.in/lltl/darray.h>
@@ -74,4 +74,4 @@ namespace lsp
     } // namespace plugui
 } // namespace lsp
 
-#endif /* PRIVATE_UI_MB_DYNA_PROCESSOR_H_ */
+#endif /* PRIVATE_UI_MB_EXPANDER_H_ */
