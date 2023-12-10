296036f1f8a82aec658c4b69a65887ce879c136b
bor-T <bor-tattoo@ya.ru>
2023-12-10 16:39:31 +0200

    UPD.

--- src/main/ui/mb_expander.cpp
+++ src/main/ui/mb_expander.cpp
@@ -2,21 +2,21 @@
  * Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
  *           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
- * This file is part of lsp-plugins-mb-dyna-processor
+ * This file is part of lsp-plugins-mb-expander
  * Created on: 25 нояб. 2020 г.
  *
- * lsp-plugins-mb-dyna-processor is free software: you can redistribute it and/or modify
+ * lsp-plugins-mb-expander is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * lsp-plugins-mb-dyna-processor is distributed in the hope that it will be useful,
+ * lsp-plugins-mb-expander is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with lsp-plugins-mb-dyna-processor. If not, see <https://www.gnu.org/licenses/>.
+ * along with lsp-plugins-mb-expander. If not, see <https://www.gnu.org/licenses/>.
  */
 
  #include <lsp-plug.in/plug-fw/ui.h>
