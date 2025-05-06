67c2fdb1bcc2f130d294bfc3ed921595f5373962
Vladimir Sadovnikov <sadko4u@gmail.com>
2025-05-07 02:22:27 +0300

    Updated metadata

--- src/main/meta/mb_expander.cpp
+++ src/main/meta/mb_expander.cpp
@@ -168,7 +168,7 @@ namespace lsp
 
         #define MB_COMMON(bands) \
             BYPASS, \
-            COMBO("mode", "Expander mode", 1, mb_global_mb_exp_modes), \
+            COMBO("mode", "Expander mode", "Mode", 1, mb_global_mb_exp_modes), \
             AMP_GAIN("g_in", "Input gain", mb_expander_metadata::IN_GAIN_DFL, 10.0f), \
             AMP_GAIN("g_out", "Output gain", mb_expander_metadata::OUT_GAIN_DFL, 10.0f), \
             AMP_GAIN("g_dry", "Dry gain", 0.0f, 10.0f), \
@@ -177,32 +177,32 @@ namespace lsp
             LOG_CONTROL("react", "FFT reactivity", "Reactivity", U_MSEC, mb_expander_metadata::FFT_REACT_TIME), \
             AMP_GAIN("shift", "Shift gain", 1.0f, 100.0f), \
             LOG_CONTROL("zoom", "Graph zoom", "Zoom", U_GAIN_AMP, mb_expander_metadata::ZOOM), \
-            COMBO("envb", "Envelope boost", mb_expander_metadata::FB_DEFAULT, mb_exp_sc_boost), \
-            COMBO("bsel", "Band selection", mb_expander_metadata::SC_BAND_DFL, bands)
+            COMBO("envb", "Envelope boost", "Env boost", mb_expander_metadata::FB_DEFAULT, mb_exp_sc_boost), \
+            COMBO("bsel", "Band selection", "Band selector", mb_expander_metadata::SC_BAND_DFL, bands)
 
-        #define MB_CHANNEL(id, label) \
-            SWITCH("flt" id, "Band filter curves" label, 1.0f), \
+        #define MB_CHANNEL(id, label, alias) \
+            SWITCH("flt" id, "Band filter curves" label, "Show flt" alias, 1.0f), \
             MESH("ag" id, "Expander amplitude graph " label, 2, mb_expander_metadata::FFT_MESH_POINTS)
 
         #define MB_SPLIT(id, label, alias, enable, freq) \
-            SWITCH("cbe" id, "Expander band enable" label, enable), \
+            SWITCH("cbe" id, "Expander band enable" label, "Split on " alias, enable), \
             LOG_CONTROL_DFL("sf" id, "Split frequency" label, "Split" alias, U_HZ, mb_expander_metadata::FREQ, freq)
 
         #define MB_BAND_COMMON(id, label, alias, x, total, fe, fs) \
-            COMBO("scm" id, "Sidechain mode" label, mb_expander_metadata::SC_MODE_DFL, mb_exp_sc_modes), \
+            COMBO("scm" id, "Sidechain mode" label, "SC mode" alias, mb_expander_metadata::SC_MODE_DFL, mb_exp_sc_modes), \
             CONTROL("sla" id, "Sidechain lookahead" label, U_MSEC, mb_expander_metadata::LOOKAHEAD), \
             LOG_CONTROL("scr" id, "Sidechain reactivity" label, "SC react" alias, U_MSEC, mb_expander_metadata::REACTIVITY), \
             AMP_GAIN100("scp" id, "Sidechain preamp" label, GAIN_AMP_0_DB), \
-            SWITCH("sclc" id, "Sidechain custom lo-cut" label, 0), \
-            SWITCH("schc" id, "Sidechain custom hi-cut" label, 0), \
-            LOG_CONTROL_DFL("sclf" id, "Sidechain lo-cut frequency" label, "SC LCF" label, U_HZ, mb_expander_metadata::FREQ, fe), \
-            LOG_CONTROL_DFL("schf" id, "Sidechain hi-cut frequency" label, "SC HCF" label, U_HZ, mb_expander_metadata::FREQ, fs), \
+            SWITCH("sclc" id, "Sidechain custom lo-cut" label, "SC LCF on" alias, 0), \
+            SWITCH("schc" id, "Sidechain custom hi-cut" label, "SC HCF on" alias, 0), \
+            LOG_CONTROL_DFL("sclf" id, "Sidechain lo-cut frequency" label, "SC LCF" alias, U_HZ, mb_expander_metadata::FREQ, fe), \
+            LOG_CONTROL_DFL("schf" id, "Sidechain hi-cut frequency" label, "SC HCF" alias, U_HZ, mb_expander_metadata::FREQ, fs), \
             MESH("bfc" id, "Side-chain band frequency chart" label, 2, mb_expander_metadata::MESH_POINTS + 4), \
             \
-            COMBO("em" id, "Expander mode" label, mb_expander_metadata::EM_DEFAULT, mb_exp_modes), \
-            SWITCH("ee" id, "Expander enable" label, 1.0f), \
-            SWITCH("bs" id, "Solo band" label, 0.0f), \
-            SWITCH("bm" id, "Mute band" label, 0.0f), \
+            COMBO("em" id, "Expander mode" label, "Mode " alias, mb_expander_metadata::EM_DEFAULT, mb_exp_modes), \
+            SWITCH("ee" id, "Expander enable" label, "On" alias, 1.0f), \
+            SWITCH("bs" id, "Solo band" label, "Solo" alias, 0.0f), \
+            SWITCH("bm" id, "Mute band" label, "Mute" alias, 0.0f), \
             LOG_CONTROL("al" id, "Attack threshold" label, "Att thresh" alias, U_GAIN_AMP, mb_expander_metadata::ATTACK_LVL), \
             LOG_CONTROL("at" id, "Attack time" label, "Att time" alias, U_MSEC, mb_expander_metadata::ATTACK_TIME), \
             LOG_CONTROL("rrl" id, "Release threshold" label, "Rel thresh" alias, U_GAIN_AMP, mb_expander_metadata::RELEASE_LVL), \
@@ -222,44 +222,44 @@ namespace lsp
             METER_OUT_GAIN("rlm" id, "Reduction level meter" label, GAIN_AMP_P_24_DB)
 
         #define MB_MONO_BAND(id, label, alias, x, total, fe, fs) \
-            COMBO("sce" id, "External sidechain source" label, 0.0f, mb_exp_sc_type), \
+            COMBO("sce" id, "External sidechain source" label, "Ext SC src" alias, 0.0f, mb_exp_sc_type), \
             MB_BAND_COMMON(id, label, alias, x, total, fe, fs)
 
         #define MB_STEREO_BAND(id, label, alias, x, total, fe, fs) \
-            COMBO("sce" id, "External sidechain source" label, 0.0f, mb_exp_sc_type), \
-            COMBO("scs" id, "Sidechain source" label, 0, mb_exp_sc_source), \
-            COMBO("sscs" id, "Split sidechain source" label, 0, mb_exp_sc_split_source), \
+            COMBO("sce" id, "External sidechain source" label, "Ext SC src" alias, 0.0f, mb_exp_sc_type), \
+            COMBO("scs" id, "Sidechain source" label, "SC source" alias, 0, mb_exp_sc_source), \
+            COMBO("sscs" id, "Split sidechain source" label, "SC split" alias, 0, mb_exp_sc_split_source), \
             MB_BAND_COMMON(id, label, alias, x, total, fe, fs)
 
         #define MB_SPLIT_BAND(id, label, alias, x, total, fe, fs) \
-            COMBO("sce" id, "External sidechain source" label, 0.0f, mb_exp_sc_type), \
-            COMBO("scs" id, "Sidechain source" label, 0, mb_exp_sc_source), \
+            COMBO("sce" id, "External sidechain source" label, "Ext SC src" alias, 0.0f, mb_exp_sc_type), \
+            COMBO("scs" id, "Sidechain source" label, "SC source" alias, 0, mb_exp_sc_source), \
             MB_BAND_COMMON(id, label, alias, x, total, fe, fs)
 
         #define MB_SC_MONO_BAND(id, label, alias, x, total, fe, fs) \
-            COMBO("sce" id, "External sidechain source" label, 0.0f, mb_exp_sc_type_sc), \
+            COMBO("sce" id, "External sidechain source" label, "Ext SC src" alias, 0.0f, mb_exp_sc_type_sc), \
             MB_BAND_COMMON(id, label, alias, x, total, fe, fs)
 
         #define MB_SC_STEREO_BAND(id, label, alias, x, total, fe, fs) \
-            COMBO("sce" id, "External sidechain source" label, 0.0f, mb_exp_sc_type_sc), \
-            COMBO("scs" id, "Sidechain source" label, 0, mb_exp_sc_source), \
-            COMBO("sscs" id, "Split sidechain source" label, 0, mb_exp_sc_split_source), \
+            COMBO("sce" id, "External sidechain source" label, "Ext SC src" alias, 0.0f, mb_exp_sc_type_sc), \
+            COMBO("scs" id, "Sidechain source" label, "SC source" alias, 0, mb_exp_sc_source), \
+            COMBO("sscs" id, "Split sidechain source" label, "SC split" alias, 0, mb_exp_sc_split_source), \
             MB_BAND_COMMON(id, label, alias, x, total, fe, fs)
 
         #define MB_SC_SPLIT_BAND(id, label, alias, x, total, fe, fs) \
-            COMBO("sce" id, "External sidechain source" label, 0.0f, mb_exp_sc_type_sc), \
-            COMBO("scs" id, "Sidechain source" label, 0, mb_exp_sc_source), \
+            COMBO("sce" id, "External sidechain source" label, "Ext SC src" alias, 0.0f, mb_exp_sc_type_sc), \
+            COMBO("scs" id, "Sidechain source" label, "SC source" alias, 0, mb_exp_sc_source), \
             MB_BAND_COMMON(id, label, alias, x, total, fe, fs)
 
         #define MB_STEREO_CHANNEL \
-            SWITCH("flt", "Band filter curves", 1.0f), \
+            SWITCH("flt", "Band filter curves", "Show filters", 1.0f), \
             MESH("ag_l", "Expander amplitude graph Left", 2, mb_expander_metadata::FFT_MESH_POINTS), \
             MESH("ag_r", "Expander amplitude graph Right", 2, mb_expander_metadata::FFT_MESH_POINTS), \
-            SWITCH("ssplit", "Stereo split", 0.0f)
+            SWITCH("ssplit", "Stereo split", "Stereo split", 0.0f)
 
-        #define MB_FFT_METERS(id, label) \
-            SWITCH("ife" id, "Input FFT graph enable" label, 1.0f), \
-            SWITCH("ofe" id, "Output FFT graph enable" label, 1.0f), \
+        #define MB_FFT_METERS(id, label, alias) \
+            SWITCH("ife" id, "Input FFT graph enable" label, "FFT In" alias, 1.0f), \
+            SWITCH("ofe" id, "Output FFT graph enable" label, "FFT Out" alias, 1.0f), \
             MESH("ifg" id, "Input FFT graph" label, 2, mb_expander_metadata::FFT_MESH_POINTS + 2), \
             MESH("ofg" id, "Output FFT graph" label, 2, mb_expander_metadata::FFT_MESH_POINTS)
 
@@ -273,8 +273,8 @@ namespace lsp
             PORTS_MONO_PLUGIN,
             MB_EXP_SHM_LINK_MONO,
             MB_COMMON(exp_sc_bands),
-            MB_CHANNEL("", ""),
-            MB_FFT_METERS("", ""),
+            MB_CHANNEL("", "", ""),
+            MB_FFT_METERS("", "", ""),
             MB_CHANNEL_METERS("", ""),
 
             MB_SPLIT("_1", " 1", " 1", 0.0f, 40.0f),
@@ -312,9 +312,9 @@ namespace lsp
             MB_EXP_SHM_LINK_STEREO,
             MB_COMMON(exp_sc_bands),
             MB_STEREO_CHANNEL,
-            MB_FFT_METERS("_l", " Left"),
+            MB_FFT_METERS("_l", " Left", " L"),
             MB_CHANNEL_METERS("_l", " Left"),
-            MB_FFT_METERS("_r", " Right"),
+            MB_FFT_METERS("_r", " Right", " R"),
             MB_CHANNEL_METERS("_r", " Right"),
 
             MB_SPLIT("_1", " 1", " 1", 0.0f, 40.0f),
@@ -360,11 +360,11 @@ namespace lsp
             PORTS_STEREO_PLUGIN,
             MB_EXP_SHM_LINK_STEREO,
             MB_COMMON(exp_sc_lr_bands),
-            MB_CHANNEL("_l", " Left"),
-            MB_CHANNEL("_r", " Right"),
-            MB_FFT_METERS("_l", " Left"),
+            MB_CHANNEL("_l", " Left", " L"),
+            MB_CHANNEL("_r", " Right", " R"),
+            MB_FFT_METERS("_l", " Left", " L"),
             MB_CHANNEL_METERS("_l", " Left"),
-            MB_FFT_METERS("_r", " Right"),
+            MB_FFT_METERS("_r", " Right", " R"),
             MB_CHANNEL_METERS("_r", " Right"),
 
             MB_SPLIT("_1l", " 1 Left", " 1 L", 0.0f, 40.0f),
@@ -427,11 +427,11 @@ namespace lsp
             PORTS_STEREO_PLUGIN,
             MB_EXP_SHM_LINK_STEREO,
             MB_COMMON(exp_sc_ms_bands),
-            MB_CHANNEL("_m", " Mid"),
-            MB_CHANNEL("_s", " Side"),
-            MB_FFT_METERS("_m", " Mid"),
+            MB_CHANNEL("_m", " Mid", " M"),
+            MB_CHANNEL("_s", " Side", " S"),
+            MB_FFT_METERS("_m", " Mid", " M"),
             MB_CHANNEL_METERS("_l", " Left"),
-            MB_FFT_METERS("_s", " Side"),
+            MB_FFT_METERS("_s", " Side", " S"),
             MB_CHANNEL_METERS("_r", " Right"),
 
             MB_SPLIT("_1m", " 1 Mid", " 1 M", 0.0f, 40.0f),
@@ -495,8 +495,8 @@ namespace lsp
             PORTS_MONO_SIDECHAIN,
             MB_EXP_SHM_LINK_MONO,
             MB_COMMON(exp_sc_bands),
-            MB_CHANNEL("", ""),
-            MB_FFT_METERS("", ""),
+            MB_CHANNEL("", "", ""),
+            MB_FFT_METERS("", "", ""),
             MB_CHANNEL_METERS("", ""),
 
             MB_SPLIT("_1", " 1", " 1", 0.0f, 40.0f),
@@ -535,9 +535,9 @@ namespace lsp
             MB_EXP_SHM_LINK_STEREO,
             MB_COMMON(exp_sc_bands),
             MB_STEREO_CHANNEL,
-            MB_FFT_METERS("_l", " Left"),
+            MB_FFT_METERS("_l", " Left", " L"),
             MB_CHANNEL_METERS("_l", " Left"),
-            MB_FFT_METERS("_r", " Right"),
+            MB_FFT_METERS("_r", " Right", " R"),
             MB_CHANNEL_METERS("_r", " Right"),
 
             MB_SPLIT("_1", " 1", " 1", 0.0f, 40.0f),
@@ -584,11 +584,11 @@ namespace lsp
             PORTS_STEREO_SIDECHAIN,
             MB_EXP_SHM_LINK_STEREO,
             MB_COMMON(exp_sc_lr_bands),
-            MB_CHANNEL("_l", " Left"),
-            MB_CHANNEL("_r", " Right"),
-            MB_FFT_METERS("_l", " Left"),
+            MB_CHANNEL("_l", " Left", " L"),
+            MB_CHANNEL("_r", " Right", " R"),
+            MB_FFT_METERS("_l", " Left", " L"),
             MB_CHANNEL_METERS("_l", " Left"),
-            MB_FFT_METERS("_r", " Right"),
+            MB_FFT_METERS("_r", " Right", " R"),
             MB_CHANNEL_METERS("_r", " Right"),
 
             MB_SPLIT("_1l", " 1 Left", " 1 L", 0.0f, 40.0f),
@@ -652,11 +652,11 @@ namespace lsp
             PORTS_STEREO_SIDECHAIN,
             MB_EXP_SHM_LINK_STEREO,
             MB_COMMON(exp_sc_ms_bands),
-            MB_CHANNEL("_m", " Mid"),
-            MB_CHANNEL("_s", " Side"),
-            MB_FFT_METERS("_m", " Mid"),
+            MB_CHANNEL("_m", " Mid", " M"),
+            MB_CHANNEL("_s", " Side", " S"),
+            MB_FFT_METERS("_m", " Mid", " M"),
             MB_CHANNEL_METERS("_l", " Left"),
-            MB_FFT_METERS("_s", " Side"),
+            MB_FFT_METERS("_s", " Side", " S"),
             MB_CHANNEL_METERS("_r", " Right"),
 
             MB_SPLIT("_1m", " 1 Mid", " 1 M", 0.0f, 40.0f),
